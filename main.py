8aee330f40ae4c7b5f1ab3585cf6f8059e5af9a3
Kiran Deshmukh <kirandeshmukh1337@gmail.com>
2024-10-25 12:38:28 +0530

    sampled to create a basic bigram model

--- main.py
+++ main.py
@@ -1,2 +1,54 @@
-words = open('names.txt','r').read().splitlines()
-words[:10]
+import torch
+import matplotlib.pyplot as plt
+words = open('names.txt', 'r').read().splitlines()
+
+b = {}
+
+N = torch.zeros(27, 27, dtype=torch.int32)
+
+chars = sorted(list(set(''.join(words))))
+stoi = {s: i+1 for i, s in enumerate(chars)}
+stoi['.'] = 0
+stoi
+itos = {i: s for s, i in stoi.items()}
+
+for w in words:
+    chs = ['.'] + list(w) + ['.']
+    for ch1, ch2 in zip(chs, chs[1:]):
+        ix1 = stoi[ch1]
+        ix2 = stoi[ch2]
+        N[ix1, ix2] += 1
+
+
+# visualizing
+plt.figure(figsize=(16, 16))
+plt.imshow(N, cmap='Blues')
+for i in range(27):
+    for j in range(27):
+        chstr = itos[i] + itos[j]
+        plt.text(j, i, chstr, ha="center", va="bottom", color='gray')
+        plt.text(j, i, N[i, j].item(), ha="center", va="top", color='gray')
+plt.axis('off')
+
+
+# sampling the model
+
+g = torch.Generator().manual_seed(2147483647)
+
+for i in range(20):
+    out = []
+    ix = 0
+    while True:
+        p = N[ix].float()
+        p = p/p.sum()
+
+        
+
+        ix = torch.multinomial(
+            p, num_samples=1, replacement=True, generator=g).item()
+
+        out.append(itos[ix])
+        if ix == 0:
+            break
+
+    print(''.join(out))
